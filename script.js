0fc4894a2795108cca5a717587e7cbd98334c1fa
SANDHYA681 <sandhyatimsina438@gmail.com>
2024-07-22 15:02:03 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// console.log("Bye world")
+// var bottle="water"
+// //to print we use console log in js
+// //--watch if we use this it automatoic runs the code if we change too
+// console.log(bottle)
+// var age="18"
+// console.log(age)
+// var is_Nepali="true"
+// console.log(is_Nepali)
+
+// var name="Sandhya"
+// console.log("My name is",name)
+
+//task 5 friends storing data
+var name1="Sandhya"
+var name2="Adit"
+var name3="prakriti"
+var name4="Manisha"
+var name5="shristi"
+
+console.log(name1)
+console.log(name2)
+console.log(name3)
+console.log(name4)
+console.log(name5)
+//if similar kinds of data we can store it in array like
+var friends=["saA","BB","CC","DD","EE"]
+console.log(friends.length)
+console.log(friends)
+
+//to print out selective output
+var me=[{
+    name:"sandy",
+    age:18
+
+},
+{
+    name:"saandhya",
+    age:18
+
+
+}]
+console.log(me.age)
+console.log(me[0].name,me[1].age)
+
+
+// var me2=[{
+//     name:"saa",
+//     age:17
+// },
+// {
+//     name:["SANDY","AA", "LL"],
+//     age:20
+// }]
+// console.log(me2[1].name[2])
+
+
+var data={
+    data:[
+{
+    name:["Manish","Anish", "pranjal"],
+    age:20
+
+}
+    ]
+}
+
+console.log(data.data[0].name[1])
+
